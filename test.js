e9ba6b074da741ec0af011452cf49e37dad622ff
sru8 <65797261+sru8@users.noreply.github.com>
2022-03-06 17:48:52 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,8 @@
+function show_productDetails(id) {
+    document.getElementById(id).classList.replace("hide", "product-details");
+
+}
+
+function close_productList(id) {
+    document.getElementById(id).classList.add("hide");
+}
